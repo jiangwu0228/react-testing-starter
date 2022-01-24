1c5ff7aec7dca853e51a56f8dc24eb80cb349e7f
Jason Wu <jiangwu0228@gmail.com>
2022-01-24 17:37:06 +1300

    integration test
    simply combine two of unit test.

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -25,9 +25,11 @@ import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
 
 test("if an amount and note is entered, the pay button becomes enabled", async () => {
   render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
+  
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
+  
   userEvent.type(screen.getByPlaceholderText(/amount/i), "10");
   userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner");
 
-  expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
   expect(await screen.findByRole("button", { name: /pay/i })).toBeEnabled();
 });
